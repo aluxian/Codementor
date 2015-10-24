07caf3bbc2d640bae50463cae613ac4729e79351
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-10-24 21:48:22 +0300

    Simplifies item_chatroom

--- app/src/main/java/com/aluxian/codementor/Constants.java
+++ app/src/main/java/com/aluxian/codementor/Constants.java
@@ -5,7 +5,7 @@ import android.text.TextUtils;
 public class Constants {
 
     public static final String FIREBASE_URL = "https://codementor.firebaseio.com/";
-    public static final String SERVER_API_URL = "https://api.codementor.io/";
+    public static final String SERVER_API_URL = "https://www.codementor.io/api/";
 
     public static final String CODEMENTOR_SIGN_IN_URL = "https://www.codementor.io/users/sign_in";
     public static final String CODEMENTOR_FIREBASE_TOKEN_URL = "https://www.codementor.io/terms";
